daa1fafcf8be5b4e47317d61be8178b49de7c3bd
javaclz2022 <javaclz2022@gmail.com>
2022-05-02 17:59:58 +0900

    2022-05-01 PointList, CircleList 만들기

--- src/com/javaex/ex01/CircleList.java
+++ src/com/javaex/ex01/CircleList.java
@@ -1,26 +1,26 @@
 package com.javaex.ex01;
 
-public class PointList {
+public class CircleList {
 
 	// 필드
-	private Point[] pArray;
+	private Circle[] cArray;
 	private int crtPos;  //다음에 추가해줄 방번호
 	
 	// 생성자
-	public PointList() {
+	public CircleList() {
 		//배열3개 --> 원래는 만들지 않음
-		pArray = new Point[3];
+		cArray = new Circle[3];
 		crtPos = 0;
 	}
 	
 	// 메소드 gs
 
 	// 메소드 일반
-	public void add(Point point){
+	public void add(Circle circle){
 		//현재배열갯수+1
 		//기존배열의 내용은 그대로 복사
 		//마지막방에 point를 대입
-		pArray[crtPos] = point;
+		cArray[crtPos] = circle;
 		crtPos = crtPos + 1;  //crtPos++
 	}
 	
@@ -28,10 +28,10 @@ public class PointList {
 		return crtPos;
 	}
 	
-	public Point get(int index) {
+	public Circle get(int index) {
 		
-		return pArray[index];
+		return cArray[index];
 	}
-
-
+	
+	
 }
--- src/com/javaex/ex01/MyListApp.java
+++ src/com/javaex/ex01/MyListApp.java
@@ -19,23 +19,51 @@ public class MyListApp {
 		}
 		*/
 		
+		///////////////////////////////////////
+		//Point 관리
+		PointList pList = new PointList();
 		
+		Point p01 = new Point(2,3);
+		Point p02 = new Point(12,13);
 		
+		pList.add(p01);
+		pList.add(p02);
 		
+		//Point 갯수
+		System.out.println(pList.size());
 		
+		//[1]방의 y값
+		Point p = pList.get(1);
+		System.out.println(p.getY());
 		
+		//전체 출력toString();
+		for(int i=0; i<pList.size(); i++) {
+			System.out.println(pList.get(i).toString());
+		}
 		
+		System.out.println("----------------------------");
+		///////////////////////////////////////
+		//Circle 관리
+		CircleList cList = new CircleList();
 		
+		Circle c01 = new Circle(5);
+		Circle c02 = new Circle(9);
 		
+		cList.add(c01);
+		cList.add(c02);
+		//cList.add(p01);
 		
+		//Circle 갯수
+		System.out.println(cList.size());
 		
+		//[1]방의 반지름
+		Circle c = cList.get(1);
+		System.out.println(c.getRadius());
 		
-		
-		
-		
-		
-		
-		
+        //전체출력toString();		
+		for(int i=0; i<cList.size(); i++) {
+			System.out.println(cList.get(i).toString());
+		}
 		
 		
 	}
